8345781e7014d27345758dcddbd7075f2075ba61
Philippe2205 <132678771+Philippe2205@users.noreply.github.com>
2023-08-21 11:45:57 +0200

    ajout message erreur connexion

--- controllers/user.js
+++ controllers/user.js
@@ -22,17 +22,13 @@ exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
-        return res
-          .status(401)
-          .json({ message: "Paire login/mot de passe incorrecte" });
+        return res.status(401).json({ message: "Paire login/mot de passe incorrecte" });
       }
       bcrypt
         .compare(req.body.password, user.password) // Vérification du login
         .then((valid) => {
           if (!valid) {
-            return res
-              .status(401)
-              .json({ message: "Paire login/mot de passe incorrecte" });
+            return res.status(401).json({ message: "Paire login/mot de passe incorrecte" });
           }
           res.status(200).json({
             userId: user._id,
